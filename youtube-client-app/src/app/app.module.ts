6efc85729a319fda432980b55470772813e303d5
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-03-17 16:37:09 +0300

    feat: add response model interfaces

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,22 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SearchResultsComponent } from './components/search/search-results/search-results.component';
-import { SearchItemComponent } from './components/search/search-item/search-item.component';
+import HeaderComponent from './components/header/header.component';
+import SearchResultsComponent from './components/search/search-results/search-results.component';
+import SearchItemComponent from './components/search/search-item/search-item.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    SearchResultsComponent,
-    SearchItemComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
+  declarations: [AppComponent, HeaderComponent, SearchResultsComponent, SearchItemComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
-export default class AppModule { }
+export default class AppModule {}
--- youtube-client-app/src/app/model/search-response.model.ts
+++ youtube-client-app/src/app/model/search-response.model.ts
@@ -1,2 +1,13 @@
+import { ISearchItem } from './search-item.model';
+
 export interface ISearchResponse {
+  kind: string;
+  etag: string;
+  pageInfo: TPageInfo;
+  items: Array<ISearchItem>;
 }
+
+type TPageInfo = {
+  totalResults: number;
+  resultsPerPage: number;
+};
