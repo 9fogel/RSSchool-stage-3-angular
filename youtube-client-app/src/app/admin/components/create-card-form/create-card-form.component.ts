93c13cf056c705f1f6103ed3b0c1993c9a2d683b
Vera Kirayonak <Vera_Kirayonak@epam.com>
2023-04-19 15:49:34 +0300

    refactor: refactor form assignment according to mentor comments

--- youtube-client-app/src/app/admin/components/create-card-form/create-card-form.component.ts
+++ youtube-client-app/src/app/admin/components/create-card-form/create-card-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { createDateValidator } from 'src/app/shared/custom-validators';
 import { Store } from '@ngrx/store';
 import * as appActions from 'src/app/redux/actions/app.actions';
@@ -10,56 +10,67 @@ import { Router } from '@angular/router';
   templateUrl: './create-card-form.component.html',
   styleUrls: ['./create-card-form.component.scss'],
 })
-export default class CreateCardFormComponent implements OnInit {
-  createCardForm?: FormGroup;
+export default class CreateCardFormComponent {
+  createCardForm = this.formBuilder.group({
+    title: [
+      '',
+      {
+        validators: [Validators.required, Validators.minLength(3), Validators.maxLength(20)],
+      },
+    ],
+    description: [
+      '',
+      {
+        validators: [Validators.required, Validators.maxLength(255)],
+      },
+    ],
+    imageLink: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'),
+        ],
+      },
+    ],
+    videoLink: [
+      '',
+      {
+        validators: [
+          Validators.required,
+          Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'),
+        ],
+      },
+    ],
+    creationDate: [
+      '',
+      {
+        validators: [Validators.required, createDateValidator()],
+      },
+    ],
+  });
 
   constructor(private formBuilder: FormBuilder, private store: Store, private router: Router) {}
 
-  ngOnInit() {
-    this.createCardForm = this.formBuilder.group({
-      title: [
-        '',
-        {
-          validators: [Validators.required, Validators.minLength(3), Validators.maxLength(20)],
-        },
-      ],
-      description: [
-        '',
-        {
-          validators: [Validators.required, Validators.maxLength(255)],
-        },
-      ],
-      imageLink: [
-        '',
-        {
-          validators: [
-            Validators.required,
-            Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'),
-          ],
-        },
-      ],
-      videoLink: [
-        '',
-        {
-          validators: [
-            Validators.required,
-            Validators.pattern('(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?'),
-          ],
-        },
-      ],
-      creationDate: [
-        '',
-        {
-          validators: [Validators.required, createDateValidator()],
-        },
-      ],
-    });
-  }
-
   submitCreateCardForm(): void {
     if (this.createCardForm?.valid) {
-      this.store.dispatch(appActions.createCard({ customCard: this.createCardForm.value }));
-      this.router.navigateByUrl('/admin/cards');
+      if (
+        this.createCardForm.value.title &&
+        this.createCardForm.value.description &&
+        this.createCardForm.value.imageLink &&
+        this.createCardForm.value.videoLink &&
+        this.createCardForm.value.creationDate
+      ) {
+        const formValue = {
+          title: this.createCardForm.value.title,
+          description: this.createCardForm.value.description,
+          imageLink: this.createCardForm.value.imageLink,
+          videoLink: this.createCardForm.value.videoLink,
+          creationDate: this.createCardForm.value.creationDate,
+        };
+        this.store.dispatch(appActions.createCard({ customCard: formValue }));
+        this.router.navigateByUrl('/admin/cards');
+      }
     }
   }
 }
